df2a1c5e9aa4d929719c35acd86ecbc99f1ef0ac
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2019-01-03 04:38:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,25 @@
 #write your code here
 
-  def countdown(count)
-    while count >0
-      puts "#{count} seconds!"
-      count-=1
-    end
-      puts "HAPPY NEW YEAR!"
+ def countdown(num)
+ 
+  while num > 0
+ 
+    puts "#{num} SECOND(S)!"
+   num -= 1
+ 
   end
-
-  def countdown_with_sleep(count)
-    while count > 0
-      count-=1
-      sleep(1)
-    end
-      puts "HAPPY NEW YEAR!"
+   "HAPPY NEW YEAR!"
+ end
+ 
+  
+  def countdown_with_sleep(num)
+ 
+  while num > 0
+ 
+    puts "#{num} SECOND(S)!"
+   sleep(1)
+   num -= 1
+ 
   end
+   "HAPPY NEW YEAR!"
+ end	 end
